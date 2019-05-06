59ac45371d02e3a10d2e2cd10f50097baa451bbc
zxh719333938 <719333938@qq.com>
2019-05-06 05:57:10 +0000

    登陆成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title','默认值') - Weibo App</title>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
+
   <body>
     @include('layouts._header')
 
@@ -14,5 +15,7 @@
         @include('layouts._footer')
       </div>
     </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
