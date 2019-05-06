aa0c48fed96776d9bfcb7347bee5d02dec8ff2ac
zxh719333938 <719333938@qq.com>
2019-05-06 02:59:04 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -30,7 +30,17 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        // User::create返回一个模型实列 且route方法会自动获取第二个参数对应模型的主键
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show', [$user]);
     }
 }
 
